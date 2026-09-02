78ae532dee26e391449dda65decc1634f9d6ee82
Nahuel-Tapia <nahuelpvp1210@gmail.com>
2026-09-02 10:26:56 -0300

    fix: add entrypoint to vercel.json for backend service and export app handler

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,11 @@ import mongoose from 'mongoose';
 import configureDependencies from './configure_dependencies.js';
 import configureMiddlewares from './middlewares/configure_middlewares.js';
 
-if (!config.jwtKey) {
-  console.error(`No se ha definido un jwtKey en la configuración. Por favor 
-cree un archivo config.local.js según se especifica en config.js.`);
-  process.exit(1);
+if (config.dbConnection) {
+  mongoose.connect(config.dbConnection)
+    .then(() => console.log('Conexión exitosa a MongoDB'))
+    .catch(error => console.error('Error al conectar a MongoDB:', error));
 }
-
-mongoose.connect(config.dbConnection)
-  .then(() => console.log('Conexión exitosa a MongoDB'))
-  .catch(error => console.error('Error al conectar a MongoDB:', error));
 
 const app = express();
 const router = express.Router();
@@ -21,9 +17,14 @@ app.use('/api', router);
 configureMiddlewares(router);
 configureDependencies();
 
-app.listen(
-  config.port,
-  () => {
-    console.log(`Servidor corriendo en http://localhost:${config.port}`);
-  }
-);
+// En entornos locales se inicia app.listen, en Vercel Serverless se exporta app
+if (process.env.VERCEL !== '1') {
+  app.listen(
+    config.port,
+    () => {
+      console.log(`Servidor corriendo en http://localhost:${config.port}`);
+    }
+  );
+}
+
+export default app;
